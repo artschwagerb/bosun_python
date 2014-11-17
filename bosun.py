08f1cbb0b947f71823a0cb11fcd84124931bcb3e
Brian Artschwager <artschwagerb@users.noreply.github.com>
2014-11-17 15:05:25 -0500

    Removed implementation of code at end of class.

--- bosun.py
+++ bosun.py
@@ -42,10 +42,3 @@ class bosun_client:
 		r = requests.post(self.url, data=json.dumps(payload), headers=headers)
 
 		return r.text
-
-#Implementation
-bosun = bosun_client()
-bosun.debug = True
-tags = {"tag1": "value1","tag2": "value2"}
-result = bosun.write_data("test.testvalue1",42,tags)
-print result
